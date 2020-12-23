4bfea22d29fa116863c497a3ff4a55dd1adf9baa
Titov Stepan <titovstepan@mail.ru>
2020-12-24 00:15:33 +0500

    update setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='visit_counter',
       description='Visit counter that counted visits on your site',
       packages=find_packages(),
       test_suite='tests',
-      install_requires=['pygame==1.9.6', 'pytest'],
+      install_requires=['pymysql==0.9.3','Flask==1.1.1', 'pytest'], 
       entry_points={
           'console_scripts': ['visit_counter=visit_counter.__main__']
       }
